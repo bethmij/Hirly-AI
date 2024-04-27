6d95445e6d1db2776212e5a487d3f54920821d3f
bethmij <bethmij@gmail.com>
2024-04-27 17:31:21 +0530

    config routing

--- hirly-AI-frontend/src/App.jsx
+++ hirly-AI-frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import './App.css'
+
+
 
 function App() {
 
-  return (
-    <>
-      <Button variant="destructive">Click Me</Button>
-    </>
-  )
+    return (
+        <>
+            <Button variant="destructive">Click Me</Button>
+        </>
+    )
 }
 
 export default App

16fddc946f01da06cf11e723343132b8b57c964a
bethmij <bethmij@gmail.com>
2024-06-16 21:48:26 +0530

    tailwind css added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+
+        <div className="border-4 border-amber-600 ms-52 mt-52 bg-amber-600 w-52 h-72 z-50">
+            {/*<h1 className="text-7xl accent-red-700 ms-40 mt-60 z-0">hi</h1>*/}
+        </div>
+
+    )
 }
 
 export default App

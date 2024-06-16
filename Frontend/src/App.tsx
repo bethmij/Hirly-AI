018ce979f8736fed4c8172a3bd36dc42729531b0
bethmij <bethmij@gmail.com>
2024-06-16 22:00:56 +0530

    shadcn added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
 
     return (
-
-        <div className="border-4 border-amber-600 ms-52 mt-52 bg-amber-600 w-52 h-72 z-50">
-            {/*<h1 className="text-7xl accent-red-700 ms-40 mt-60 z-0">hi</h1>*/}
-        </div>
-
+        <>
+            <Button>Click Me</Button>
+        </>
     )
 }
 

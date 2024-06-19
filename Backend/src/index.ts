0c88ac0c75b307c1a8e46a30cb9399b8db3ca33e
bethmij <bethmij@gmail.com>
2024-06-19 09:13:30 +0530

    env added

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import express from "express";
 import jobRouter from "./api/jobs";
 import {connectionDB} from "./infrastructure/db";
--- Backend/src/infrastructure/db.ts
+++ Backend/src/infrastructure/db.ts
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 export const connectionDB = async () => {
 
     try {
-        const connectionString = "mongodb+srv://bethmij:Bethmi20020530.@cluster0.qsftct6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        const connectionString = process.env.MONGODB_URI
+        // const connectionString = "mongodb+srv://bethmij:Bethmi20020530.@cluster0.qsftct6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        if(!connectionString){
+            throw new Error("Please add the connection string")
+        }
+
         await mongoose.connect(connectionString)
         console.log("DB Connection Successful!")
 

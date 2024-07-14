ea9ff3df02b3b1bd4ecf9e2cae78e4bd3168ebf3
bethmij <bethmij@gmail.com>
2024-07-14 09:55:36 +0530

    adding global error handling

---
+++ Backend/src/api/middleware/global-error-handler.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+
+const GlobalErrorHandlingMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
+    switch (error.name) {
+        case "NotFoundError":
+            console.error(error)
+            return res.status(404).json({ message: error.message.replaceAll("\n", "") });
+
+        case "ValidationError":
+            console.error(error)
+            return res.status(400).json({ message: error.message.replaceAll("\n", "") });
+
+
+        case "Unauthenticated":
+            console.error(error)
+            return res.status(401).json({ message: error.message.replaceAll("\n", "") });
+
+        default:
+            console.error(error)
+            return res.status(500).json({ message: error.message });
+    }
+}
+
+export default GlobalErrorHandlingMiddleware
